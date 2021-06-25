0956a2b3dc7e63992220751c0ac9396e8697c2b2
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-06-25 00:33:21 -0300

    exercicio 2 - somar todos elementos do array

--- fundamentos/dia-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/percorrendoArrays.js
+++ fundamentos/dia-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/percorrendoArrays.js
@@ -1,6 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
- for(let wichNumber of numbers){
-     console.log(wichNumber)
+let somaTodos = 0;
+ for(let index = 0; index < numbers.length; index += 1){
+     
+     somaTodos += numbers[index];
  }
+ console.log(somaTodos)
 
