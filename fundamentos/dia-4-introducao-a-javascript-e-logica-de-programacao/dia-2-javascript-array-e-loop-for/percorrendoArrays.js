02fb72f89e229614b525a1a6c7a6670901bce85b
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-06-25 01:33:06 -0300

    exercicio 5 - ordenando uma lista em ordem crescente e pegando o mairo elemento

--- fundamentos/dia-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/percorrendoArrays.js
+++ fundamentos/dia-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/percorrendoArrays.js
@@ -1,14 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let somaTodos = 0;
- for(let index = 0; index < numbers.length; index += 1){
-     
-     somaTodos += numbers[index];
- }
- let mediaAritmetica = somaTodos / numbers.length;
- 
- if(mediaAritmetica > 20){
-     console.log('valor maior que 20');
- }else{
-     console.log('valor menor ou igual a 20')
- }
+
+//alinhar uma nva função com os valores em ordem crescente
+//tomar cuidado que o sort sozinho alinha unicode diferente.
+let orderNumber = numbers.sort((a, b) => a - b);
+
+//usar os atributos aprendidos para pegar o ultimo valor que ficou sendo o maior.
+console.log(orderNumber[orderNumber.length-1]);
 
