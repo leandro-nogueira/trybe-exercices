d8003f3498fabd4aa0ad850ea27d7fc200e5599d
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-06-28 20:58:35 -0300

    eita bagunça

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logica-e-algoritmos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logica-e-algoritmos.js
@@ -7,6 +7,7 @@
 //     line = line + symbol;
     
 // }
+// console.log(line);
 
 // for(let index = 0; index < n; index += 1){
 //     console.log(line)
@@ -23,3 +24,13 @@
         
 // }
 
+
+
+let n = 5;
+let symbol = '*';
+let line = '';
+for(let index = 0; index < n; index += 1){
+    line = line + symbol;
+    console.log(line)
+        
+}
