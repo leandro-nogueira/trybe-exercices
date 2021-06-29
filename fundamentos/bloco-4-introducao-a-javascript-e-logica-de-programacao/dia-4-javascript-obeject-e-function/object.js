2e0f75b9ce588a812d6990c53399dbe0af3c66bc
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-06-29 11:45:13 -0300

    exercício concatenando os valores e propriedades dos objetos.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-obeject-e-function/object.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-obeject-e-function/object.js
@@ -4,18 +4,29 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-// console.log('bem vindo : '+info['personagem']);
-// console.log('quem é vc? ');
-// console.log(info.nota);
+// // console.log('bem vindo : '+info['personagem']);
+// // console.log('quem é vc? ');
+// // console.log(info.nota);
 
 info.recorrente = 'Sim';
 
-// console.log(info)
+// // console.log(info)
 
-// for(let chave in info){
-//   console.log(chave);  
-// };
+// // for(let chave in info){
+// //   console.log(chave);
+// // }
 
+
+
+let comparacao = {
+  personagem: "Tio Patinhas",
+  origin: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota:"O último MacPatinhas ",
+  recorrente: "sim"
+};
 for(let chave in info){
-  console.log(info[chave])
+  
+  console.log(info[chave] + ' e ' + comparacao[chave]);
+
 }
+  
