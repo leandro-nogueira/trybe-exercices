b79452b1d917304861c97783ee5cb415b68ca007
Leandro <leandro2903.lan@gmail.com>
2021-06-28 22:26:59 -0300

    colocando varias variaveis encapsuladas num unico objeto

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-obeject-e-function/object.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-obeject-e-function/object.js
@@ -4,5 +4,21 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log("Bem-vinda, " + info.personagem)
+// console.log("Bem-vinda, " + info.personagem)
 
+
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let jogadora = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {golden: 2,
+  silver: 3}
+  
+};
+
+console.log(jogadora)
