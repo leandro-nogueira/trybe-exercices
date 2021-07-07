589bb428de1550a143fcf241ab2b8db9aeb99445
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-07 01:29:54 -0300

    separei em pastas para exercíciosde fixação da aula e exercícios do dia !

--- exercises-fixacao/bloco_5/dia_1/script.js
+++ exercises-fixacao/bloco_5/dia_1/script.js
@@ -1,3 +1,15 @@
-document.getElementById("container").style.backgroundColor= 'lightgray';
-document.getElementById("header-container").style.backgroundColor= 'green';
+document.getElementById('header-container').style.backgroundColor='green';
 
+document.getElementsByTagName('body')[0].style.backgroundColor='lightgray';
+
+document.querySelectorAll('.emergency-tasks')[0].style.backgroundColor="pink";
+
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor="yellow";
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor="purple"
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor="purple"
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor="black";
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor="black";
+
+document.getElementById('footer-container').style.backgroundColor="darkgreen"
--- exercises/bloco_5/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("container").style.backgroundColor= 'lightgray';
-document.getElementById("header-container").style.backgroundColor= 'green';
-
