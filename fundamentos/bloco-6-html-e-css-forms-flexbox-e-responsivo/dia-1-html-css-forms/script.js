0365a9fba19b5994008ed40c5c832e0c1c01d053
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-15 08:36:25 -0300

    colocando os estados nas options dinamicamente pelo javascript

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,14 @@
+function createStateOptions () {
+  const stateSigla = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+  const stateLista = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+  
+  const estado = document.querySelector('#estado');
+  
+    for (let i = 0; i < stateLista.length; i += 1 ) {
+      let stateName = document.createElement('option');
+      stateName.value = stateSigla[i];
+      stateName.innerText = stateLista[i];
+      document.querySelector('#estado').appendChild(stateName);
+    }
+}
+createStateOptions ()
