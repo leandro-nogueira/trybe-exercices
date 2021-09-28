18358a6353899fc18b0cf167608fb33b97e4b2d8
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-27 21:22:28 -0300

    mudando de brach para estudos

---
+++ front-end/bloco-11-inroducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import pokemons from "../data"
+import Pokemon from "./Pokemon";
+
+class Pokedex extends React.Component {
+  render() {
+    const {pokemons} = this.props;
+    return pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)
+  }
+}
+
+export default Pokedex;
