51fb0e93973ed12f8c939424a101435b51eee179
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-28 14:54:03 -0300

    alterando o state de forma assíncrona

--- front-end/refazendo-bloco-12-react/funcy-buttons/src/App.js
+++ front-end/refazendo-bloco-12-react/funcy-buttons/src/App.js
@@ -2,32 +2,42 @@ import React from 'react';
 import './App.css'
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
+  constructor() {
+    super();
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0,
+    }
 
-  //   }
-
-  // }
+  }
 
   handleChange1 = () => {
-    console.log('cheguei 1')
+    this.setState((previewState, _props) => ({
+      numeroDeCliques1: previewState.numeroDeCliques1 + 1,
+    }))
   }
   handleChange2 = () => {
-    console.log('cheguei 2')
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1,
+    }))
   }
-  handleChange3 = () => {
-    console.log('cheguei 3')
+  handleChange3 = () => {    
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1,
+    }))
   }
+  
 
 
   render() {
+    const { numeroDeCliques1, numeroDeCliques2, numeroDeCliques3 } = this.state;
     return (
       <div className="App-header">
         <div>
-          <button onClick={ this.handleChange1 } >Meu Botão 1</button>  
-          <button onClick={ this.handleChange2 } >Meu Botão 2</button>  
-          <button onClick={ this.handleChange3 } >Meu Botão 3</button>  
+          <button onClick={ this.handleChange1 } >{ numeroDeCliques1 }</button>  
+          <button onClick={ this.handleChange2 } >{ numeroDeCliques2 }</button>  
+          <button onClick={ this.handleChange3 } >{ numeroDeCliques3 }</button>  
         </div>
       </div>
     )
